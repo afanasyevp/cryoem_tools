42ff5801c2bd2f3085ecf207a6fc172d2c16a8fc
Pavel Afanasyev <afanasyev.p.v@gmail.com>
2024-03-06 11:12:31 +0100

    Plots fsc for xml files of relion and cryosparc

--- plot_fsc.py
+++ plot_fsc.py
@@ -1,80 +1,125 @@
-#!/Applications/anaconda/bin/python
+#!/usr/bin/env python3
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-ver=200604
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
 import sys
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator, FixedFormatter
+from pathlib import Path
 import argparse
-import xml.etree.ElementTree as ET
-import numpy as np
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-def import_fscFile_relion(fscFile, pix):
-    fscVal=[]
+PROG = Path(__file__).name
+VER = 20240306
+TOPHALFLINE = ("=" * 35)
+UNDERLINE = ("=" * 70) + ("=" * (len(PROG) + 2))  # line for the output
+COLOR='#4bd1a0'
+
+
+def import_fsc_xml(filename, pix):
+    '''
+    Reads a standard .xml files of FSC (EMDB format) and returns a single dictionary of FSC values for building the curve
+
+    For a single-curve cistem file outputs a fsc dictionary:
+    {"curve_name": "fsc_01",
+    
+    "fsc": [list_of_fsc_values],
+    "res": [list_of_res_values],
+    "frac_of_nq" [list_of_frac_of_nq_val] }
+    '''
+    fsc=[]
     res=[]
-    fracOfNq=[]
-    xmldata = minidom.parse("%s" %fscFile)
+    frac_of_nq=[]
+    xmldata = minidom.parse(filename)
     fsc_items_list=xmldata.getElementsByTagName("coordinate")
-    #print(fsc_items_list)
+    curve={}
     for i in fsc_items_list:
         x = i.getElementsByTagName("x")
         y = i.getElementsByTagName("y")
-        fscVal.append(float(y[0].childNodes[0].nodeValue))
+        fsc.append(float(y[0].childNodes[0].nodeValue))
         res.append(1/float(x[0].childNodes[0].nodeValue))
-        fracOfNq.append(2*pix*float(x[0].childNodes[0].nodeValue) )
-    return fscVal, res, fracOfNq
+        frac_of_nq.append(2*pix*float(x[0].childNodes[0].nodeValue) )
+    curve["curve_name"] = filename          
+    curve["fsc"] = fsc
+    curve["res"] = res
+    curve["frac_of_nq"] = frac_of_nq
+    return curve
        
-def import_fscFile_cistem(fscFile, pix):
-    f1=open(fscFile, 'Ur')
-    lines = f1.readlines()
-    fscVal=[]
-    res=[]
-    fracOfNq=[]
-    for line in lines[5:-1]:
-        values=line.split()
-        res.append(float(values[1]))
-        fscVal.append(float(values[4]))
-        fracOfNq.append(2*pix/float(values[1]))
-    f1.close()
-    return fscVal, res, fracOfNq
-   
-def main():
-    output_text='''
+def import_fsc_cistem(filename, pix):
+    '''
+    For a single-curve cistem file outputs a fsc dictionary:
+    {"curve_name": "fsc_01",
+    
+    "fsc": [list_of_fsc_values],
+    "res": [list_of_res_values],
+    "frac_of_nq" [list_of_frac_of_nq_val] }
+    '''
+    # TODO: implement reading files with multiple classes 
+    curve={}
+    with open(filename, 'Ur') as f1:
+        lines = f1.readlines()
+        fsc=[]
+        res=[]
+        frac_of_nq=[]
+        for line in lines[5:-1]:
+            values=line.split()
+            res.append(float(values[1]))
+            fsc.append(float(values[4]))
+            frac_of_nq.append(2*pix/float(values[1]))
+        curve["curve_name"] = filename
+        curve["fsc"] = fsc
+        curve["res"] = res
+        curve["frac_of_nq"] = frac_of_nq
+    return [curve]
 
-======================================== plot_fsc.py ============================================
- 
-Returns FSC from the cisTEM fsc.txt file or relion postprocess_fsc.xml
+def import_fsc_cryosparc(filename, pix):
+    '''
+    For a single-curve cistem file outputs a fsc dictionary:
+    {"curve_name": "fsc_01",
+    
+    "fsc": [list_of_fsc_values],
+    "res": [list_of_res_values],
+    "frac_of_nq" [list_of_frac_of_nq_val] }
 
-Example for cisTEM: plot_fsc.py --i fsc1.txt fsc2.txt fsc3.txt --pix 1.09 
-Example for relion: plot_fsc.py --i postprocess_fsc.xml --pix 1.09
+    Cryosparc txt first line: wave_number	fsc_nomask	fsc_sphericalmask	fsc_loosemask	fsc_tightmask	fsc_noisesub_raw	fsc_noisesub_true	fsc_noisesub
+    ''' 
+    curve={}
+    with open(filename, 'Ur') as f:
+        lines = f.readlines()
+        fsc=[]
+        res=[]
+        frac_of_nq=[]
+        column_names=lines[0].split()
+        for line in lines[1:]:
+            values=line.split()
+            res.append(float(values[1]))
+            fsc.append(float(values[4]))
+            frac_of_nq.append(2*pix/float(values[1]))
+        curve["curve_name"] = filename
+        curve["fsc"] = fsc
+        curve["res"] = res
+        curve["frac_of_nq"] = frac_of_nq
+    return [curve]
 
-[version %s]
-Written and tested in python3.7
-
-Pavel Afanasyev
-https://github.com/afanasyevp/cryoem_tools
-=================================================================================================
-''' % ver 
-    parser = argparse.ArgumentParser(description="")
-    add=parser.add_argument
-    add('--i', nargs="+",  help="Saved FSC table from cisTEM software")
-    add('--pix', default='1', help="Pixel size. Default value: 1 A/pix")
-    args = parser.parse_args()
-    print(output_text)
-    if len(sys.argv) == 1:
-        #parser.print_help()
-        sys.exit(2)
-    if not args.i:
-        print("No input file given.")
-        #parser.print_help()
-        sys.exit(2)
-    else:
-        fscFiles = args.i 
-    if not args.pix:
-        pix = float(args.pix)
-        print("No pixel size given. Pixel size is considered 1A/pix")
-    else:
-        pix=float(args.pix)
+def make_fsc_plots(curves):
+    '''Plots curves based from a list of dictionaries:
+    {"curve_name": "fsc_01",
+    "fsc": [list_of_fsc_values],
+    "res": [list_of_res_values],
+    "frac_of_nq" [list_of_frac_of_nq_val] }
+    '''
     plt.figure()
     plt.xlabel('Fraction of Nyquist', fontweight="bold")
     plt.ylabel('FSC', fontweight="bold")
@@ -83,27 +128,15 @@ https://github.com/afanasyevp/cryoem_tools
     plt.xlim(left=0)  # adjust the left leaving right unchanged
     plt.ylim(top=1)  # adjust the top leaving bottom unchanged
     plt.ylim(bottom=0)  # adjust the bottom leaving top unchanged
-    if len(fscFiles) == 1:
-        if fscFiles[0][-4:] == '.txt':
-            fscVal, res, fracOfNq = import_fscFile_cistem(fscFiles[0], pix)
-        elif fscFiles[0][-4:] == '.xml':
-            fscVal, res, fracOfNq = import_fscFile_relion(fscFiles[0], pix)
-        else:
-            print("ERROR: %s file is not a standard input" %fscFiles[0])
-            sys.exit(2)
-        plt.plot(fracOfNq, fscVal, color='#4bd1a0', linewidth=2)
-    else: 
-        for fscFile in fscFiles:
-            if fscFile[-4:] == '.txt':
-                fscVal, res, fracOfNq = import_fscFile_cistem(fscFile, pix)
-            elif fscFile[-4:] == '.xml':
-                fscVal, res, fracOfNq = import_fscFile_relion(fscFile, pix)
-            else:
-                print("ERROR: %s file is not a standard input" %fscFile)
-                sys.exit(2)
-            plt.plot(fracOfNq, fscVal, linewidth=2) 
+    
+    if len(curves) == 0:
+        sys.exit(" => ERROR in make_plots! No curves found!")
+    elif len(curves) == 1:
+        plt.plot(curves[0]["frac_of_nq"], curves[0]["fsc"], color=COLOR, linewidth=2)
+    else:
+        for curve in curves:
+            plt.plot(curve["frac_of_nq"], curve["fsc"], linewidth=2)
 
-    from matplotlib.ticker import FixedLocator, FixedFormatter
     ax = plt.gca()
     x_formatter = FixedFormatter(["", "", "0.2", "",  "0.4", "", "0.6", "", "0.8","", "1"])
     y_formatter = FixedFormatter(["0", "", "0.143", "0.2", "","0.4","", "0.6", "", "0.8", "","1"])
@@ -116,7 +149,81 @@ https://github.com/afanasyevp/cryoem_tools
     ax.yaxis.set_major_locator(y_locator)
     plt.grid(linestyle='-', linewidth=0.5)
     plt.show()
+
+   
+def main(fsc_files, pix, software):
+    '''
+    Files should be read into a list of dictionaries (curves)
+    {"curve_name": "fsc_01",
+    "fsc": [list_of_fsc_values],
+    "res": [list_of_res_values],
+    
+    "frac_of_nq" [list_of_frac_of_nq_val] }
+    '''
+    # controls file extensions and extracts fsc curves
+    curves=[]
+    if software == "cistem":
+        for fsc_file in fsc_files:
+            if fsc_file[-4:] != ".txt":
+                sys.exit(f" => ERROR in checking the type of cisTEM file {fsc_file}! Must be .txt")
+            curves.extend(import_fsc_cistem(fsc_file, pix))
+    elif software == "relion" or "cryosparc":
+        for fsc_file in fsc_files:
+            if fsc_file[-4:] == ".xml":
+                # relion and cryoscarc use the same EMDB .xml format
+                curves.append(import_fsc_xml(fsc_file, pix))
+            elif fsc_file[-4:] == ".dat":
+                # relion format
+                # TODO
+                ...
+            elif fsc_file[-4:] == ".txt":
+                # cryosparc format
+                # TODO
+                ...
+         
+    else: 
+        sys.exit(f" => ERROR: {software} is not a standard input")
+
+    
+    make_fsc_plots(curves)
+
     
 if __name__ == '__main__':
-    main()
+    description = f"""
+{TOPHALFLINE} {PROG} {TOPHALFLINE}
+ Builds FSC plot(s) from exported files.
+ 
+    Software   | Extension
+   ------------+-----------
+    relion     |   .xml .dat
+    cryosparc  |   .xml 
+    cisTEM     |   .txt
+
+  See full list of options with:
+  {PROG} --help
+
+ [version {VER}]
+ Written and tested in python3.11
+ Pavel Afanasyev
+ https://github.com/afanasyevp/cryoem_tools 
+{UNDERLINE}"""
+
+    examples=f"""EXAMPLES:
+ relion/cryosparc (.xml files): {PROG} --i postprocess_fsc.xml --pix 1.09
+ cistem (.txt files): {PROG} --i fsc1.txt fsc2.txt fsc3.txt --pix 1.09
+"""
+    class UltimateHelpFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+        pass
+    parser = argparse.ArgumentParser(prog=PROG, formatter_class=UltimateHelpFormatter, description=description, epilog=examples)
+    add=parser.add_argument
+    add('--i', nargs="+",  help="Saved FSC table from cisTEM software", metavar="")
+    add('--software', default="relion", choices=["relion", "cistem", "cryosparc"], help="From which software the files were exported (relion/cryosparc/cistem)", metavar="")
+    add('--pix', type=float, required=True, help="Pixel size", metavar="")
+    args = parser.parse_args()
+    print(description)
+    fsc_files=args.i
+    pix=args.pix
+    software=args.software
+
+    main(fsc_files, pix, software)
 
