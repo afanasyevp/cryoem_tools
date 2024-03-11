a1aceb54c8d2a8900032fcfec016666d7a8a2a7c
Pavel Afanasyev <afanasyev.p.v@gmail.com>
2024-03-11 23:21:04 +0100

    version 2.0

--- plot_fsc.py
+++ plot_fsc.py
@@ -14,23 +14,28 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FixedLocator, FixedFormatter
 from pathlib import Path, PurePosixPath, PurePath
 import argparse
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
-
-# import numpy as np
-# from scipy.interpolate import interp1d
-
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy import optimize
 
 PROG = Path(__file__).name
-VER = "20240310_cs50x"
+VER = 20240311
 TOPHALFLINE = "=" * 35
 UNDERLINE = ("=" * 70) + ("=" * (len(PROG) + 2))  # line for the output
 COLOR_MAIN = "#4bd1a0"
 CISTEM_FSCFILE_NUM_OF_COLUMNS = 7
+LINEWIDTH = 1
+PLOT_TITLE = ""# "Fourier Shell Correlation"
+Y_LABEL = "Fourier Shell Correlation"
+X_LABEL_FRAC_OF_NQ = "Fraction of Nyquist" 
+X_LABEL_RESOLUTION = "1/Frequency (1/Å)"
 
 
 def fraction_of_nyqist(pixel_size, reverce_frequency):
@@ -58,6 +63,7 @@ def import_fsc_xml(filename, pix):
     frac_of_nq = []
     xmldata = minidom.parse(filename)
     fsc_items_list = xmldata.getElementsByTagName("coordinate")
+    print(f" => Working on {xmldata.getElementsByTagName('fsc')[0].getAttribute('title').split()[0]} data in {filename} file\n")
     curve = {}
     for i in fsc_items_list:
         x = i.getElementsByTagName("x")
@@ -70,13 +76,14 @@ def import_fsc_xml(filename, pix):
     curve["fsc"] = fsc
     curve["res"] = res
     curve["frac_of_nq"] = frac_of_nq
-    return curve
+    return [curve]
 
 
 def import_fsc_relion_dat(filename, pix):
     """
     For a single-curve relion .dat file outputs a fsc dictionary like in import_fsc_xml
     """
+    print(f" => Working on {filename} file\n")
     with open(filename, "r") as f1:
         lines = f1.readlines()
         fsc = []
@@ -88,7 +95,7 @@ def import_fsc_relion_dat(filename, pix):
             res.append(res_value)
             fsc.append(float(values[1]))
             frac_of_nq.append(fraction_of_nyqist(pix, res_value))
-    return {"curve_name": filename, "fsc": fsc, "frac_of_nq": frac_of_nq}
+    return [{"curve_name": filename, "fsc": fsc, "frac_of_nq": frac_of_nq, "res": res}]
 
 
 def import_fsc_cistem(filename, pix):
@@ -103,6 +110,7 @@ def import_fsc_cistem(filename, pix):
     """
     curves = []
     curve = {}
+    print(f" => Working on {filename} file\n")
     with open(filename, "r") as f1:
         lines = f1.readlines()
         fsc = []
@@ -159,6 +167,7 @@ def import_fsc_cryosparc_txt(filename, pix, box):
     the last column (fsc_noisesub) is "Corrected FSC"
     Resolution = (box size * pixel size) / wave number
     """
+    print(f" => Working on {filename} file\n")
     curve_nomask = {}
     curve_sphericalmask = {}
     curve_loosemask = {}
@@ -239,7 +248,7 @@ def import_fsc_cryosparc_txt(filename, pix, box):
     return curves
 
 
-def make_fsc_plots(curves, units, pix):
+def make_fsc_plots(curves, units, pix, resolution):
     """Plots curves based from a list of dictionaries:
     {"curve_name": "fsc_01",
     "fsc": [list_of_fsc_values],
@@ -250,60 +259,112 @@ def make_fsc_plots(curves, units, pix):
         sys.exit(" => ERROR in make_plots! No curves found, check your input!")
 
     plt.figure()
-    plt.ylabel("FSC", fontweight="bold")
-    plt.title("Fourier Shell Correlation", fontweight="bold")
-    plt.ylim(top=1)  # adjust the top leaving bottom unchanged
-    plt.ylim(bottom=0)  # adjust the bottom leaving top unchanged
-    y_formatter = FixedFormatter(
-        ["0", "", "0.143", "0.2", "", "0.4", "", "0.6", "", "0.8", "", "1"]
-    )
-    y_locator = FixedLocator([0, 0.1, 0.143, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-
+    plt.ylabel(Y_LABEL, fontweight="bold")
+    plt.title(PLOT_TITLE, fontweight="bold")
+    plt.ylim(bottom=-0.1, top=1)  # adjust the bottom leaving top unchanged
+    
+    if args.threshold:
+        plt.plot([0, 1], [0.143, 0.143], linewidth=1, color="black", linestyle="dashed")
+        y_formatter = FixedFormatter(["0", "", "0.143", "0.2", "", "0.4", "", "0.6", "", "0.8", "", "1"])
+        y_locator = FixedLocator([0, 0.1, 0.143, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+    else:
+        y_formatter = FixedFormatter(["0", "", "0.2", "", "0.4", "", "0.6", "", "0.8", "", "1"])
+        y_locator = FixedLocator([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+    plt.grid(linestyle="-", linewidth=0.25)
     plt.xlim(right=1)  # adjust the right leaving left unchanged
     plt.xlim(left=0)  # adjust the left leaving right unchanged
     x_tick_values = [i * 0.1 for i in range(11)]
     x_locator = FixedLocator(x_tick_values)
 
     # 0.143 cut-off
-    plt.plot([0, 1], [0.143, 0.143], linewidth=1, color="black", linestyle="dashed")
-    plt.grid(linestyle="-", linewidth=0.5)
-
     if units == "frac_of_nq":
-        plt.xlabel("Fraction of Nyquist", fontweight="bold")
+        plt.xlabel(X_LABEL_FRAC_OF_NQ, fontweight="bold")
         x_formatter = FixedFormatter(
             ["0", "", "0.2", "", "0.4", "", "0.6", "", "0.8", "", "1"]
         )
-
+        
     elif units == "angstrom":
-        plt.xlabel("1/Frequency (1/Å)", fontweight="bold")
+        plt.xlabel(X_LABEL_RESOLUTION, fontweight="bold")
         # max value in the list of dictionaries
-        all_res_values = [res_val for curve in curves for res_val in curve["res"]]
-        max_res = max(all_res_values)
-        min_res = min(all_res_values)
-        # print(reverse_frequency(pix, max_res))
-        x_tick_values[0] = f"{2*pix/0.1:.1f}"
-        x_tick_values[1:] = [reverse_frequency(pix, val) for val in x_tick_values[1:]]
+        #all_res_values = [res_val for curve in curves for res_val in curve["res"]]
+        #max_res = max(all_res_values)
+        #min_res = min(all_res_values)
+        x_tick_values[0] = ""
+        x_tick_values[1:] = [f"{reverse_frequency(pix, val)}" for val in x_tick_values[1:]]
         x_formatter = FixedFormatter(x_tick_values)
 
-    if len(curves) == 1:
-        plt.plot(curves[0]["frac_of_nq"], curves[0]["fsc"], linewidth=1)
-    else:
         for i, curve in enumerate(curves):
-            plt.plot(
-                curve["frac_of_nq"],
-                curve["fsc"],
-                linewidth=1,
-                label=curve["curve_name"],
-                color=f"C{i}",
-            )
-            plt.legend()
+            custom_plot_command(curve, f"C{i}", args)
 
     ax = plt.gca()
     ax.xaxis.set_major_formatter(x_formatter)
-    ax.yaxis.set_major_formatter(y_formatter)
     ax.xaxis.set_major_locator(x_locator)
+    ax.yaxis.set_major_formatter(y_formatter)
     ax.yaxis.set_major_locator(y_locator)
+    plt.axhline(0, color='black', linewidth=0.5)
     plt.show()
+
+def custom_plot_command(curve, color, args, linewidth=LINEWIDTH):
+    if args.label == True:
+        if args.resolution:
+            label_text = curve["curve_name"] +  " (" + determine_resolution_0143(curve['frac_of_nq'], curve['fsc'], args.pix) + "Å)"
+        else:
+            label_text = curve["curve_name"]
+        plt.plot(curve["frac_of_nq"], curve["fsc"], linewidth=linewidth, color=color, label=label_text)
+        plt.legend() 
+    else:
+        plt.plot(curve["frac_of_nq"], curve["fsc"], linewidth=linewidth, color=color)
+
+
+def run_file_analysis(filename, args):
+    if not os.path.exists(filename):
+        sys.exit(f" => ERROR! File {filename} does not exist")
+    if filename[-4:] == ".xml":
+        return import_fsc_xml(filename, args.pix)
+    elif filename[-4:] == ".dat":
+        return import_fsc_relion_dat(filename, args.pix)
+    elif filename[-4:] == ".txt":
+        with open(filename, "r") as f:
+            first_line = f.readline()
+            if first_line.split()[0]  == "Class":
+                return import_fsc_cistem(filename, args.pix)
+            elif first_line.split()[0] == "wave_number":
+                if args.box == None:
+                    sys.exit(" => ERROR! No --box provided! Please specify your box size for cryosparc {filename} file")
+                return import_fsc_cryosparc_txt(filename, args.pix, args.box)
+            else:
+                print(f" => WARNING! File type in {filename} not detected. Considering general 2-column type like in relion.dat files")
+    else:
+        print(f" => WARNING! File type in {filename} not detected. Considering general 2-column type like in relion.dat files")
+        return import_fsc_relion_dat(filename, args.pix)
+def determine_resolution_0143(x_curve, y_curve, pix, y_threshold=0.143):
+    '''
+    For two lists determining a FSC curve determines resolution based on 0.143 threshold
+    '''
+    # Interpolate the y-values of the curve
+    f_curve = interp1d(x_curve, y_curve, fill_value='extrapolate')
+
+    # Generate x-values for the threshold curve
+    x_threshold = np.linspace(min(x_curve), max(x_curve), 1000)
+    y_threshold_values = np.full_like(x_threshold, y_threshold)
+
+    # Interpolate the y-values of the threshold curve
+    f_threshold = interp1d(x_threshold, y_threshold_values, fill_value='extrapolate')
+
+    # Find the intersection points by finding the x-values where the absolute difference between the interpolated y-values is minimized
+    intersection_x_values = []
+    try:
+        for x in x_curve:
+            intersection = optimize.root_scalar(lambda x: f_curve(x) - f_threshold(x), bracket=[min(x_curve), max(x_curve)])
+            if intersection.converged:
+                intersection_x_values.append(intersection.root)
+    except Exception as e:
+        print(" => ERROR! Error occurred during intersection calculation of resolution estimation:", e)
+        sys.exit("Consider running without --resolution option")
+
+    resolution_angstrom = 2*pix/min(intersection_x_values)
+    
+    return f'{resolution_angstrom:.1f}'
 
 
 def main(args):
@@ -312,41 +373,17 @@ def main(args):
     {"curve_name": "fsc_01",
     "fsc": [list_of_fsc_values],
     "res": [list_of_res_values],
-
+ 
     "frac_of_nq" [list_of_frac_of_nq_val] }
     """
-    software = args.software
     fsc_files = args.i
     pix = args.pix
 
     # controls file extensions and extracts fsc curves
     curves = []
-    if software == "cistem":
-        for fsc_file in fsc_files:
-            if fsc_file[-4:] != ".txt":
-                sys.exit(
-                    f" => ERROR in checking the type of cisTEM file {fsc_file}! Must be .txt"
-                )
-            curves.extend(import_fsc_cistem(fsc_file, pix))
-    elif software == "relion" or "cryosparc":
-        for fsc_file in fsc_files:
-            if fsc_file[-4:] == ".xml":
-                # relion and cryoscarc use the same EMDB .xml format
-                curves.append(import_fsc_xml(fsc_file, pix))
-            elif fsc_file[-4:] == ".dat":
-                # relion format
-                curves.append(import_fsc_relion_dat(fsc_file, pix))
-            elif fsc_file[-4:] == ".txt":
-                if software == "relion":
-                    sys.exit(
-                        ' => ERROR! Wrong input format: "--software relion" option accepts .dat and .xml files'
-                    )
-                # cryosparc .txt format
-                else:
-                    curves.extend(import_fsc_cryosparc_txt(fsc_file, pix, args.box))
-    else:
-        sys.exit(f" => ERROR: {software} is not a standard input")
-    make_fsc_plots(curves, args.units, pix)
+    for fsc_file in fsc_files:
+        curves.extend(run_file_analysis(fsc_file, args))
+    make_fsc_plots(curves, args.units, pix, args.resolution)
 
 
 if __name__ == "__main__":
@@ -377,9 +414,9 @@ if __name__ == "__main__":
 {UNDERLINE}"""
 
     examples = f"""EXAMPLES:
- relion/cryosparc (.xml files): {PROG} --i postprocess_fsc.xml --pix 1.09 --units angstrom
- cryosparc (.xml files): {PROG} --i JXXX_fsc_iteration_YYY.txt --pix 1.09 --box 420
- cistem (.txt files): {PROG} --i fsc1.txt fsc2.txt fsc3.txt --pix 1.09 --units frac_of_nq
+ relion/cryosparc (.xml files): {PROG} --i postprocess_fsc.xml --pix 1.09 --units angstrom --resolution --threshold --label
+ cryosparc (.xml files): {PROG} --i JXXX_fsc_iteration_YYY.txt --pix 1.09 --box 420 --resolution --threshold --label
+ cistem (.txt files): {PROG} --i fsc1.txt fsc2.txt fsc3.txt --pix 1.09 --units frac_of_nq --resolution --threshold --label
 """
 
     class UltimateHelpFormatter(
@@ -397,30 +434,24 @@ if __name__ == "__main__":
     )
     add = parser.add_argument
     add("--i", nargs="+", help="Saved FSC table from cisTEM software")
-    add(
-        "--software",
-        default="relion",
-        choices=["relion", "cistem", "cryosparc"],
-        help="Options: relion/cryosparc/cistem | From which software the files were exported",
-        metavar="",
-    )
     add("--pix", type=float, required=True, help="Pixel size", metavar="")
     add("--box", type=int, help="Box size for cryosparc .txt files", metavar="")
     add(
         "--units",
         choices=["frac_of_nq", "angstrom"],
-        default="frac_of_nq",
+        default="angstrom",
         help='Options: angstrom/frac_of_nq | X-axis as a "Fraction of Nyquist" or "1/Frequency"',
         metavar="",
     )
+    add("--resolution", default=False, action="store_true", help="Show resolution on the plot")
+    add("--label",  default=False, action="store_true", help="Label on the plot")
+    add("--threshold", default=False, action="store_true", help="0.143 threshold on the plot")
     args = parser.parse_args()
     print(description)
 
-    if args.software == "cryosparc":
-        for fsc_file in args.i:
-            if PurePath(fsc_file).suffix == ".txt" and args.box == None:
-                sys.exit(
-                    " => ERROR! No --box provided! Please specify your box size for cryosparc .txt file"
-                )
+    print(f"\n\n => Input parameters: ")
+    for key, value in vars(args).items():
+        print(f"  --{key}  {value}")
+    print()
 
     main(args)
