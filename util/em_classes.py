a72e1314473329422e7fc79c709418690a445fc4
Pavel Afanasyev <afanasyev.p.v@gmail.com>
2024-11-22 23:59:13 +0100

    Created TS and Tilt classes

--- util/em_classes.py
+++ util/em_classes.py
@@ -22,9 +22,9 @@ import shutil
 import subprocess
 import sys
 import os
-from pathlib import Path
+from pathlib import Path, PureWindowsPath
 
-VER=20241105
+VER=20241122
 
 class FSC:
 	pass
@@ -44,19 +44,81 @@ class Movie:
 class Micrograph:
 	pass
 
-class TS:
-    def __init__(self, options, mdocfile):
-        self.args = None
-        self.mdocfile = None
-        self.tilts = {}
-        self.general_info = {}
-        self.movies_tiff = False
 
-class Tilt(TS):
-    def __init__(self, ZValue):
-        # super().__init__(stemname, mdocfile, outputname, mdocsuffix, framespath)
-        self.NewSubFramePath = None
+class Dataset:
+    def __init__(self, name, targets=None):
+        self.name=name
+        self.data = {}  
+
+
+class TS:
+    def __init__(self, mdocfile, framespath):
+        self.mdocfile = str(Path(mdocfile).absolute())
+        self.name=Path(self.mdocfile).stem
+        self.tilts = []
+        self.general_info = {}
+        self.framespath = framespath
+    
+    def fetch_info_from_mdoc(self):
+        """
+        Fetches info from mdoc files, writes it out into Tilt instances.
+        """
+        print(f"  Fetching info from {self.mdocfile}  file...")
+        with open(self.mdocfile, "r") as f:
+            lines = f.readlines()
+            ts_start = False
+            for i, line in enumerate(lines, start=1):
+                # Assume each non-empty line has a "=" sign
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                if ts_start:
+                    line_list = line.split("=")
+                    if  line_list[0] == "[ZValue ":
+                        ZValue = int(line_list[-1].split("]")[0][1:])
+                        #print(tilt.__dict__)
+                        tilt = Tilt(self.mdocfile, ZValue)
+                        self.tilts.append(tilt)
+                    else:
+                        k = line_list[0].split()[0].strip()
+                        v = " ".join(line_list[1:])
+                        if k == "SubFramePath":
+                            """
+                            Example of the old SubFramePath: [full_path_to_the_dataset]\frames\YYMMDD_dataset_3931.tif
+                            NewSubFramePath would be: [full_path_to_frames_folder]/YYMMDD_dataset_3931.tif
+                            """
+                            if self.framespath:
+                                newdir = os.path.dirname(self.framespath)
+                                NewSubFramePath = newdir + "/" + PureWindowsPath(v).name
+                            else:
+                                NewSubFramePath = "frames/" + PureWindowsPath(v).name
+                            setattr(tilt, "NewSubFramePath", NewSubFramePath)
+                        else:
+                            v=v.split()
+                            setattr(tilt, k, v)
+                else:
+                    if "[ZValue" in line:
+                        ts_start = True
+                        line_list = line.split("=")
+                        ZValue = int(line_list[-1].split("]")[0][1:])
+                        tilt = Tilt(self.mdocfile, ZValue)
+                        self.tilts.append(tilt)
+                    else:
+                    # general header info (first ~10 lines
+                        if "=" in line and line[0] != "[":
+                            line_list = line.split("=")
+                            k = line_list[0].split()[0].strip()
+                            v = " ".join(line_list[1:])
+                            self.general_info[k] = v
+                        else:
+                            self.general_info["other_info_line_" + str(i)] = line
+            
+class Tilt:
+    def __init__(self, mdoc_file, ZValue):
+        self.mdoc_file=mdoc_file 
         self.ZValue = ZValue
+        self.NewSubFramePath = None # => to be set by TS.fetch_info_from_mdoc()
+
         self.MinMaxMean = None
         self.TiltAngle = None
         self.StagePosition = None
@@ -67,6 +129,7 @@ class Tilt(TS):
         self.DoseRate = None
         self.PixelSpacing = None
         self.SpotSize = None
+        self.ProbeMode = None
         self.Defocus = None
         self.ImageShift = None
         self.RotationAngle = None
@@ -91,4 +154,5 @@ class Tilt(TS):
         self.SpecimenShift = None
         self.EucentricOffset = None
         self.CtfFind = None
+        self.TimeStamp = None
 
